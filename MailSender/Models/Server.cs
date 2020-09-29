0ef90313606a31dd0e34ed8d96322c4089fe227f
Ivan <jaskev@yandex.ru>
2020-09-29 13:40:10 +0300

    Пакет FontAwesome + векторные иконки

---
+++ MailSender/Models/Server.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.Models
+{
+    class Server
+    {
+        public string Address { get; set; }
+    }
+}
