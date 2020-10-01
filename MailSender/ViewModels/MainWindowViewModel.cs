86844bbacb3e042da24a89008e1147544eaed225
Ivan <jaskev@yandex.ru>
2020-10-01 16:10:39 +0300

    Модель представления. Проба привязки в тестовом приложении

---
+++ MailSender/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MailSender.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        string _title = "Тестовое окно";
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (_title == value) return;
+                _title = value;
+                OnPropertyChanged("Title");            }
+        }
+    }
+}
---
+++ Tests/WpfTests/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using WpfTests.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfTests.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        string _title = "Тестовое окно";
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (_title == value) return;
+                _title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+    }
+}
