e060d404fb97d368fa802a9c1c6773ce3390723c
Ivan <jaskev@yandex.ru>
2020-10-02 13:52:45 +0300

    Контейнер сервисов и локатор

--- MailSender/App.xaml.cs
+++ MailSender/App.xaml.cs
@@ -1,8 +1,23 @@
-﻿using System.Windows;
+﻿using MailSender.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Windows;
 
 namespace MailSender
 {
     public partial class App : Application
     {
+        static IHost _hosting;
+        public static IHost Hosting => _hosting
+            ??= Host.CreateDefaultBuilder(Environment.GetCommandLineArgs())
+            .ConfigureServices(ConfigureServices)
+            .Build();
+
+        public static IServiceProvider Services => Hosting.Services;
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton<MainWindowViewModel>();
+        }
     }
 }
