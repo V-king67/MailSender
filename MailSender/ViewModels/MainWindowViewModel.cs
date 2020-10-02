a3d3da193a8076eb8d8d8dc63c354a4e18c328b5
Ivan <jaskev@yandex.ru>
2020-10-02 14:32:59 +0300

    Привязка списков к ViewModel

--- MailSender/ViewModels/MainWindowViewModel.cs
+++ MailSender/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,7 @@
-﻿using MailSender.ViewModels.Base;
+﻿using MailSender.Data;
+using MailSender.Models;
+using MailSender.ViewModels.Base;
+using System.Collections.ObjectModel;
 
 namespace MailSender.ViewModels
 {
@@ -9,6 +12,64 @@ namespace MailSender.ViewModels
         {
             get => _title;
             set => Set(ref _title, value);
+        }
+
+        ObservableCollection<Server> _servers;
+        ObservableCollection<Sender> _senders;
+        ObservableCollection<Recipient> _recipients;
+        ObservableCollection<Message> _messages;
+
+        public ObservableCollection<Server> Servers
+        {
+            get => _servers;
+            set => Set(ref _servers, value);
+        }
+        public ObservableCollection<Sender> Senders
+        {
+            get => _senders;
+            set => Set(ref _senders, value);
+        }
+        public ObservableCollection<Recipient> Recipients
+        {
+            get => _recipients;
+            set => Set(ref _recipients, value);
+        }
+        public ObservableCollection<Message> Messages
+        {
+            get => _messages;
+            set => Set(ref _messages, value);
+        }
+        public MainWindowViewModel()
+        {
+            Servers = new ObservableCollection<Server>(TestData.Servers);
+            Senders = new ObservableCollection<Sender>(TestData.Senders);
+            Recipients = new ObservableCollection<Recipient>(TestData.Recipients);
+            Messages = new ObservableCollection<Message>(TestData.Messages);
+        }
+
+        Server _selectedServer;
+        Sender _selectedSender;
+        Recipient _selectedRecipient;
+        Message _selectedMessage;
+        public Server SelectedServer
+        {
+            get => _selectedServer;
+            set => Set(ref _selectedServer, value);
+        }
+        public Sender SelectedSender
+        {
+            get => _selectedSender;
+            set => Set(ref _selectedSender, value);
+        }
+        public Recipient SelectedRecipient
+        {
+            get => _selectedRecipient;
+            set => Set(ref _selectedRecipient, value);
+        }
+        public Message SelectedMessage
+        {
+            get => _selectedMessage;
+            set => Set(ref _selectedMessage, value);
         }
     }
 }
