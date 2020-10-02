86603b042294df2c20203b5b5df4101dfd2f77e1
Ivan <jaskev@yandex.ru>
2020-10-02 18:49:16 +0300

    Отладка сервиса отправки

--- MailSender/App.xaml.cs
+++ MailSender/App.xaml.cs
@@ -20,7 +20,14 @@ namespace MailSender
         private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
         {
             services.AddSingleton<MainWindowViewModel>();
+
+#if DEBUG
+            services.AddTransient<IMailService, DebugMailService>();
+#else
             services.AddTransient<IMailService, SmtpMailService>();
+
+#endif
+
             //services.AddScoped<>();
 
             //using (var scope = Services.CreateScope())
