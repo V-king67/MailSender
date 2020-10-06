edc310567c6e520b0538afd1d4d2b1659a359b40
Ivan <jaskev@yandex.ru>
2020-10-06 12:05:19 +0300

    Валидация свойств

--- MailSender/Views/RecipientEditor.xaml.cs
+++ MailSender/Views/RecipientEditor.xaml.cs
@@ -1,26 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace MailSender.Views
 {
-    /// <summary>
-    /// Логика взаимодействия для RecipientsEditor.xaml
-    /// </summary>
     public partial class RecipientEditor : UserControl
     {
         public RecipientEditor()
         {
             InitializeComponent();
+        }
+
+        private void OnDataValidationError(object sender, ValidationErrorEventArgs e)
+        {
+            var control = (Control)sender;
+            if (e.Action == ValidationErrorEventAction.Added)
+                control.ToolTip = e.Error.ErrorContent.ToString();
+            else control.ClearValue(ToolTipProperty);
         }
     }
 }
