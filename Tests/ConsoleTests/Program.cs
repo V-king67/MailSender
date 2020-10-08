31a6b917feef105efb1cb63c743457ad26628988
Ivan <jaskev@yandex.ru>
2020-10-08 17:14:28 +0300

    Тестирую потоки

--- Tests/ConsoleTests/Program.cs
+++ Tests/ConsoleTests/Program.cs
@@ -1,5 +1,5 @@
-﻿using MailSender.lib.Interfaces;
-using MailSender.lib.Service;
+﻿using System;
+using System.Threading;
 
 namespace ConsoleTests
 {
@@ -7,12 +7,36 @@ namespace ConsoleTests
     {
         static void Main(string[] args)
         {
-            IEncryptorService cryptor = new Rfc2898Encryptor();
-            var str = "Hello world!";
-            const string password = "MailSender!";
+            var mainThread = Thread.CurrentThread;
+            var mainThreadId = mainThread.ManagedThreadId;
+            mainThread.Name = "Основной поток";
 
-            var cryptedStr = cryptor.Encrypt(str, password);
-            var decrypredStr = cryptor.Decrypt(cryptedStr, password);
+            var timerThread = new Thread(TimerMethod);
+            timerThread.Name = "Поток часов";
+            timerThread.IsBackground = true;
+            timerThread.Start();
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(10);
+            }
+        }
+
+        static void TimerMethod()
+        {
+            PrintThreadInfo();
+            while (true)
+            {
+                Console.Title = DateTime.Now.ToString("HH:mm:ss.ff");
+                Thread.Sleep(500);
+            }
+        }
+
+        static void PrintThreadInfo()
+        {
+            var thread = Thread.CurrentThread;
+            Console.WriteLine("id: {0} name: {1} priority: {2}", thread.ManagedThreadId, thread.Name, thread.Priority);
         }
     }
 }
