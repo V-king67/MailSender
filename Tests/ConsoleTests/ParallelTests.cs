a9667a81af4bae43892b1288eccc63e5a2f71672
Ivan <jaskev@yandex.ru>
2020-10-13 14:21:09 +0300

    Упрощенная TPL

---
+++ Tests/ConsoleTests/ParallelTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleTests
+{
+    class ParallelTests
+    {
+        public static void Start()
+        {
+            Parallel.Invoke(
+            new ParallelOptions { MaxDegreeOfParallelism = 2 },
+            ParallelInvokeMethod,
+            ParallelInvokeMethod,
+            ParallelInvokeMethod,
+            ParallelInvokeMethod,
+            ParallelInvokeMethod,
+            ParallelInvokeMethod,
+            ParallelInvokeMethod,
+            () => Console.WriteLine("One more method"));
+
+            Parallel.For(0, 40, new ParallelOptions { MaxDegreeOfParallelism = 2 }, i =>
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(100);
+            });
+
+            var messages = Enumerable.Range(1, 150).Select(i => $"Message {i}");
+            Parallel.ForEach(messages, message =>
+            {
+                Console.WriteLine(message);
+                Thread.Sleep(100);
+            });
+
+            var messagesCount = messages
+                .AsParallel()
+                .WithDegreeOfParallelism(2)
+                .Where(msg => msg.EndsWith("0"))
+                .AsSequential()
+                .Count();
+
+            Console.WriteLine("Сообщений с круглым номером: {0}", messagesCount);
+
+        }
+        private static void ParallelInvokeMethod()
+        {
+            Console.WriteLine("ThrID: {0} - started", Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(500);
+            Console.WriteLine("ThrID: {0} - finished", Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private static void ParallelInvokeMethod(string msg)
+        {
+            Console.WriteLine("ThrID: {0} - started: {1}", Thread.CurrentThread.ManagedThreadId, msg);
+            Thread.Sleep(500);
+            Console.WriteLine("ThrID: {0} - finished: {1}", Thread.CurrentThread.ManagedThreadId, msg);
+        }
+
+    }
+}
--- Tests/ConsoleTests/Program.cs
+++ Tests/ConsoleTests/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ConsoleTests
 {
@@ -6,11 +9,45 @@ namespace ConsoleTests
     {
         static void Main(string[] args)
         {
+
+
             //ThreadTests.Start();
             //CriticalSectionTests.Start();
-            ThreadPoolTests.Start();
+            //ThreadPoolTests.Start();
+            ParallelTests.Start();
+
             Console.WriteLine("Основной поток работу завершил!");
             Console.ReadLine();
         }
+
+    }
+
+    class MathTask //Имитируем класс Task
+    {
+        private readonly Thread _calculationThread;
+        private int _result;
+        private bool _isCompleted;
+
+        public bool IsCompleted => _isCompleted;
+        public int Result
+        {
+            get
+            {
+                if (!_isCompleted)
+                    _calculationThread.Join();
+                return _result;
+            }
+        }
+
+        public MathTask(Func<int> calculation)
+        {
+            _calculationThread = new Thread(() =>
+            {
+                _result = calculation();
+                _isCompleted = true;
+            }) { IsBackground = true };
+        }
+
+        public void Start() => _calculationThread.Start();
     }
 }
