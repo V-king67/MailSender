0908cda649f2c99574f03c06493504bb80208dad
Ivan <jaskev@yandex.ru>
2020-10-21 18:00:39 +0300

    Работа с файлом и отмена операции

--- Tests/WpfTests/MainWindow.xaml.cs
+++ Tests/WpfTests/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Threading;
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace WpfTests
@@ -13,33 +19,105 @@ namespace WpfTests
             InitializeComponent();
         }
 
-        private void ComputeBtnClick(object sender, RoutedEventArgs e)
+        private async void OnOpenFileClick(object sender, RoutedEventArgs e)
         {
-            new Thread(() =>
-            {
-                var result = GetResultHard();
-                //Application.Current.Dispatcher.Invoke(() => TbResult.Text = result);
-                UpdateResultValue(result);
-            })
-            { IsBackground = true }.Start();
-        }
+            //ThreadId == 1 (MainThread)
+            await Task.Yield(); //Waiting for UI
+            //ThreadId == 1
 
-        void UpdateResultValue(string result)
-        {
-            if (Dispatcher.CheckAccess())
-                TbResult.Text = result;
-            else
-                Dispatcher.Invoke(() => UpdateResultValue(result));
-        }
-
-        string GetResultHard()
-        {
-            for (int i = 0; i < 500; i++)
+            var dialog = new OpenFileDialog
             {
-                Thread.Sleep(10);
+                Title = "Выберите файл для чтения",
+                Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*",
+                RestoreDirectory = true
+            };
+
+            if (dialog.ShowDialog() != true) return;
+
+            openFile.IsEnabled = false;
+            cancelReading.IsEnabled = true;
+
+            _readingFileCancellation = new CancellationTokenSource();
+            var cancel = _readingFileCancellation.Token;
+            IProgress<double> progress = new Progress<double>(p => progressInfo.Value = p);
+
+            try
+            {
+                tbResult.Text = "Считаем слова...";
+                var count = await GetWordsCountAsync(dialog.FileName, progress, cancel);
+                tbResult.Text = $"Число слов {count}";
+            }
+            catch (OperationCanceledException)
+            {
+                Debug.WriteLine("Операция чтения файла была отменена.");
+                tbResult.Text = "---";
+                progress.Report(0);
             }
 
-            return "Hello world";
+            cancelReading.IsEnabled = false;
+            openFile.IsEnabled = true;
         }
+
+        private static async Task<int> GetWordsCountAsync(string fileName, IProgress<double> progress = null, CancellationToken cancel = default)
+        {
+            var dict = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+            cancel.ThrowIfCancellationRequested();
+            using (var reader = new StreamReader(fileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    cancel.ThrowIfCancellationRequested();
+                    var line = await reader.ReadLineAsync().ConfigureAwait(false);
+                    var words = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    //Thread.Sleep(100);
+                    await Task.Delay(100);
+
+                    foreach (var word in words)
+                        if (dict.ContainsKey(word))
+                            dict[word]++;
+                        else dict.Add(word, 1);
+
+                    progress?.Report(reader.BaseStream.Position / (double)reader.BaseStream.Length);
+                }
+            }
+            return dict.Count;
+        }
+
+        private CancellationTokenSource _readingFileCancellation;
+        private void OnCancelReadingClick(object sender, RoutedEventArgs e)
+        {
+            _readingFileCancellation?.Cancel();
+        }
+
+
+
+        //private void ComputeBtnClick(object sender, RoutedEventArgs e)
+        //{
+        //    new Thread(() =>
+        //    {
+        //        var result = GetResultHard();
+        //        //Application.Current.Dispatcher.Invoke(() => TbResult.Text = result);
+        //        UpdateResultValue(result);
+        //    })
+        //    { IsBackground = true }.Start();
+        //}
+
+        //void UpdateResultValue(string result)
+        //{
+        //    if (Dispatcher.CheckAccess())
+        //        TbResult.Text = result;
+        //    else
+        //        Dispatcher.Invoke(() => UpdateResultValue(result));
+        //}
+
+        //string GetResultHard()
+        //{
+        //    for (int i = 0; i < 500; i++)
+        //    {
+        //        Thread.Sleep(10);
+        //    }
+
+        //    return "Hello world";
+        //}
     }
 }
