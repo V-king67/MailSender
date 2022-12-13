85f50e18781b5a0be4a60ff30d23107dd1996624
Ivan <jaskev@yandex.ru>
2022-12-13 17:31:44 +0300

    Валидация модели с помощью интерфейса IDataErrorInfo

--- MailSender.lib/Models/Sender.cs
+++ MailSender.lib/Models/Sender.cs
@@ -1,6 +1,32 @@
 ﻿using MailSender.lib.Models.Base;
+using System.ComponentModel;
 
 namespace MailSender.lib.Models
 {
-    public class Sender : Person { }
+    public class Sender : Person, IDataErrorInfo//, INotifyDataErrorInfo //Динамическая проверка, построенная на событии
+    {
+        string IDataErrorInfo.Error { get; } = null;
+
+        string IDataErrorInfo.this[string PropertyName]
+        {
+            get
+            {
+                switch (PropertyName)
+                {
+                    default: return null;
+
+                    case nameof(Name):
+                        var name = Name;
+                        if (name is null) return "Имя не может быть пустой строкой";
+                        if (name.Length < 2) return "Имя не должно быть короче 2 символов";
+                        if (name.Length > 20) return "Имя не может быть длиннее 20 символов";
+
+                        return null;
+
+                    case nameof(Address):
+                        return null;
+                }
+            }
+        }
+    }
 }
