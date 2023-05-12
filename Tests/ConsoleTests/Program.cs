09ff7c318ae8cb2739570d6ed81a9e5302d99407
Ivan <jaskev@yandex.ru>
2023-05-12 20:04:43 +0300

    Передача информации (параметров) в поток

--- Tests/ConsoleTests/Program.cs
+++ Tests/ConsoleTests/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata;
 using System.Threading;
 
 namespace ConsoleTests
@@ -11,15 +12,34 @@ namespace ConsoleTests
             var mainThreadId = mainThread.ManagedThreadId;
             mainThread.Name = "main";
 
-            var timerThread = new Thread(Timer);
-            timerThread.IsBackground = true; //Поток завершится по завершении главного потока
+            var timerThread = new Thread(Timer)
+            {
+                IsBackground = true, //Поток завершится по завершении главного потока
+                Name = "Timer"
+            };
             timerThread.Start();
 
-            for (int i = 0; i < 500; i++)
+            var printerThread = new Thread(PrintMessage)
+            {
+                IsBackground = true,
+                Name = "Parameter printer"
+            };
+            printerThread.Start("Hello world!"); //Для методов с одним параметром, параметр передается в метод Start(parameter)
+
+            var message = "Hello world~!";
+            var count = 10;
+            new Thread(() => PrintMessage(message, count)) { IsBackground = true, Name = "MultiParameter printer"}.Start();
+
+            //Создание потока в классе
+            var printTask = new PrintMessageTask("Message from class", count);
+            printTask.Start();
+
+
+            /*for (int i = 0; i < 500; i++)
             {
                 Console.WriteLine("Главный поток {0}", i);
                 Thread.Sleep(10);
-            }
+            }*/
 
             Console.WriteLine("Главный поток работу закончил!");
             Console.ReadLine();
@@ -27,6 +47,7 @@ namespace ConsoleTests
 
         static void Timer()
         {
+            Console.WriteLine("Поток, обрабатывающий таймер: ");
             PrintThreadInfo();
             while (true)
             {
@@ -40,6 +61,62 @@ namespace ConsoleTests
         {
             var thread = Thread.CurrentThread;
             Console.WriteLine("id: {0}; name: {1}; priority: {2}", thread.ManagedThreadId, thread.Name, thread.Priority);
+        }
+
+        static void PrintMessage(object parameter)
+        {
+            PrintThreadInfo();
+            var msg = (string)parameter;
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("id: {0}\t{1}", threadId, msg);
+                Thread.Sleep(10);
+            }
+        }
+
+        static void PrintMessage(string message, int count)
+        {
+            PrintThreadInfo();
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine("id: {0}\t{1}", threadId, message);
+                Thread.Sleep(10);
+            }
+        }
+    }
+
+    //Архитектурно правильный способ
+    class PrintMessageTask
+    {
+        private readonly string message;
+        private readonly int count;
+        private Thread thread;
+
+        public PrintMessageTask(string message, int count)
+        {
+            this.message = message;
+            this.count = count;
+            thread = new Thread(ThreadMethod) { IsBackground = true, Name = "Incapsulated printer" };
+        }
+
+        public void Start()
+        {
+            if (thread?.IsAlive == false)
+                thread?.Start();
+        }
+
+        void ThreadMethod()
+        {
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine("id: {0}\t{1}", threadId, message);
+                Thread.Sleep(10);
+            }
+
+            thread = null; //Повторный запуск невозможен
         }
     }
 }
