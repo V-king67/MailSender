90eda9aa68d6e9b956584d2bfe3e5b0d87199a0e
Ivan <jaskev@yandex.ru>
2024-01-30 12:13:49 +0300

    Чтение и обработка файла в основном потоке (долго)

--- Tests/WpfTests/MainWindow.xaml.cs
+++ Tests/WpfTests/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading;
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
 using System.Windows;
 
 namespace WpfTests
@@ -13,33 +17,6 @@ namespace WpfTests
             InitializeComponent();
         }
 
-        private void ComputeButton_Click(object sender, RoutedEventArgs e)
-        {
-            /*var result = LengthyWork();
-            TBComputingResult.Text = result;*/     //Выполнение в главном потоке заблокирует интерфейс
-
-            //new Thread(() => TBComputingResult.Text = LengthyWork()).Start();     //При этом состояние интерфейса нельзя изменить во вторичном потоке
-
-            //Выполняем вычисление во вторичном потоке, затем используем диспетчер для доступа к основному потоку
-            new Thread(() =>
-            {
-                var result = LengthyWork();
-                //Application.Current.Dispatcher.Invoke(() => TBComputingResult.Text = result);
-                UpdateResultValue(result);
-            })
-            { IsBackground = true }.Start();
-        }
-
-        void UpdateResultValue(string result)
-        {
-            //Проверяем, находимся ли мы в одном потоке с интерфейсом (главном)
-            if (Dispatcher.CheckAccess()) 
-                TBComputingResult.Text = result;    //Алгоритм изменения интерфейса, который нужно исполнить
-            //Если нет, запускаем этот метод в главном потоке через диспетчер
-            else
-                Dispatcher.Invoke(() => UpdateResultValue(result));
-        }
-
         string LengthyWork()
         {
             for (int i = 0; i < 100; i++)
@@ -48,6 +25,38 @@ namespace WpfTests
             }
 
             return "Calculation complete";
+        }
+
+        private void OnOpenFileClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog()
+            {
+                Title = "Выбор файла для чтения",
+                Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*",
+                
+            };
+
+            if (dialog.ShowDialog() != true) return;
+
+            var dict = new Dictionary<string, int> (StringComparer.OrdinalIgnoreCase);
+
+            using (var reader = new StreamReader(dialog.FileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var words = line.Split(' ');
+                    Thread.Sleep (1000);
+
+                    foreach (var word in words)
+                        if (dict.ContainsKey(word))
+                            dict[word]++;
+                        else
+                            dict.Add(word, 1);
+                }
+            }
+
+            Result.Text = $"Word count {dict.Count}";
         }
     }
 }
