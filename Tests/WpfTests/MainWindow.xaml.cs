735de5a2cb5a94299df63667188e5e1cc239f25a
Ivan <jaskev@yandex.ru>
2024-02-20 14:50:37 +0300

    Использование async/await с пользовательским интерфесом

--- Tests/WpfTests/MainWindow.xaml.cs
+++ Tests/WpfTests/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace WpfTests
@@ -27,8 +29,13 @@ namespace WpfTests
             return "Calculation complete";
         }
 
-        private void OnOpenFileClick(object sender, RoutedEventArgs e)
+        async void OnOpenFileClick(object sender, RoutedEventArgs e)
         {
+            //Мы находимся в ThreadId = 1
+            await Task.Yield(); //Даем время на обработку сообщений UI
+            //Мы снова в ThreadId = 1
+                // --- Возвращает в основной поток только из основного потока; если изначально находились не в основном потоке, вернет в случайный поток из пула
+
             var dialog = new OpenFileDialog()
             {
                 Title = "Выбор файла для чтения",
@@ -38,25 +45,63 @@ namespace WpfTests
 
             if (dialog.ShowDialog() != true) return;
 
-            var dict = new Dictionary<string, int> (StringComparer.OrdinalIgnoreCase);
+            StartAction.IsEnabled = false;
+            CancelAction.IsEnabled = true;
 
-            using (var reader = new StreamReader(dialog.FileName))
+            readingFileCancellation = new CancellationTokenSource();
+
+            var cancel = readingFileCancellation.Token;
+            IProgress<double> progress = new Progress<double>(p => ProgressInfo.Value = p);
+            try
+            {
+
+                var count = await GetWordsCountAsync(dialog.FileName, progress, cancel);
+                Result.Text = $"Word count {count}";
+            }
+            catch (OperationCanceledException)
+            {
+                Debug.WriteLine("File reading operation was cancelled");
+                Result.Text = "-----";
+                progress.Report(0);
+            }
+            CancelAction.IsEnabled = false;
+            StartAction.IsEnabled = true;
+        }
+
+        static async Task<int> GetWordsCountAsync(string fileName, IProgress<double> progress = null, CancellationToken cancel = default)
+        {
+            var dict = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+            cancel.ThrowIfCancellationRequested();
+            using (var reader = new StreamReader(fileName))
             {
                 while (!reader.EndOfStream)
                 {
-                    var line = reader.ReadLine();
+                    cancel.ThrowIfCancellationRequested();
+                    var line = await reader.ReadLineAsync()
+                        .ConfigureAwait(false); // если false - метод продолжается в случайном потоке из пула   
+                                                // -- ConfigureAwait(false) обязательно использовать в библиотечных методах / технических методах, не работающих с интерфейсом --
+                                                // если true (по умолчанию) - возвращается в главный поток
                     var words = line.Split(' ');
-                    Thread.Sleep (1000);
+                    //Thread.Sleep (1000);
+                    await Task.Delay(400);
 
                     foreach (var word in words)
                         if (dict.ContainsKey(word))
                             dict[word]++;
                         else
                             dict.Add(word, 1);
+                    progress?.Report(reader.BaseStream.Position / (double)reader.BaseStream.Length);
                 }
             }
 
-            Result.Text = $"Word count {dict.Count}";
+            return dict.Count;
+        }
+
+        CancellationTokenSource readingFileCancellation;
+
+        private void OnCancelReadingClick(object sender, RoutedEventArgs e)
+        {
+            readingFileCancellation?.Cancel();
         }
     }
 }
