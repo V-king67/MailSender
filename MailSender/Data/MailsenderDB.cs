63b8f5f301f0bbd631af4a1c2f6a27065f9ba175
Ivan <jaskev@yandex.ru>
2024-09-03 13:56:49 +0300

    Контекст базы данных

---
+++ MailSender/Data/MailsenderDB.cs
@@ -0,0 +1,16 @@
+﻿using MailSender.lib.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MailSender.Data
+{
+    internal class MailsenderDB : DbContext
+    {
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Recipient> Recipients { get; set; }
+        public DbSet<Sender> Senders { get; set; }
+        public DbSet<Server> Servers { get; set; }
+
+        public MailsenderDB(DbContextOptions options) : base(options) { }
+        
+    }
+}
---
+++ MailSender/Data/MailsenderDBFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace MailSender.Data
+{
+    internal class MailsenderDBFactory : IDesignTimeDbContextFactory<MailsenderDB>
+    {
+        public MailsenderDB CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MailsenderDB>();
+            const string connectionStr = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Mailsender.DB;Integrated Security=True";
+            optionsBuilder.UseSqlServer(connectionStr);
+
+            return new MailsenderDB(optionsBuilder.Options);
+        }
+    }
+}
