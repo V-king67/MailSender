cda0959814329c7e3036e492ac3b783b21b39cc3
Иван <jaskev@yandex.ru>
2024-10-15 12:05:03 +0300

    Перевод тестовых данных в статический класс

--- MailSender/Data/TestData.cs
+++ MailSender/Data/TestData.cs
@@ -7,68 +7,41 @@ using System.Xml.Serialization;
 
 namespace MailSender.Data
 {
-    public class TestData
+    public static class TestData
     {
-        public List<Sender> Senders { get; set; }
-
-        public List<Recipient> Recipients { get; set; }
-
-        public List<Server> Servers { get; set; }
-
-        public List<Message> Messages { get; set; }
-
-        public static TestData LoadFromXML(string fileName)
-        {
-            var serializer = new XmlSerializer(typeof(TestData));
-            using var file = File.OpenText(fileName);
-            var result = (TestData)serializer.Deserialize(file);
-            return result;
-        }
-        public void SaveToXML(string fileName)
-        {
-            var serializer = new XmlSerializer(typeof(TestData));
-            using var file = File.Create(fileName);
-            serializer.Serialize(file, this);
-        }
-
-        public TestData TestInitialize()
-        {
-            return new TestData
+        public static List<Sender> Senders { get; } = Enumerable.Range(1, 10)
+            .Select(i => new Sender
             {
-                Senders = new List<Sender>(
-                    Enumerable.Range(1, 10)
-                    .Select(i => new Sender
-                    {
-                        Name = $"Отправитель {i}",
-                        Address = $"Sender_{i}@server.ru"
-                    }).ToList()),
+                Name = $"Отправитель {i}",
+                Address = $"sender_{i}@server.ru"
+            })
+            .ToList();
 
-                Recipients = new List<Recipient>(
-                    Enumerable.Range(1, 10)
-                    .Select(i => new Recipient
-                    {
-                        Name = $"Получатель {i}",
-                        Address = $"Recipient_{i}@server.ru"
-                    }).ToList()),
+        public static List<Recipient> Recipients { get; } = Enumerable.Range(1, 10)
+            .Select(i => new Recipient
+            {
+                Name = $"Получатель {i}",
+                Address = $"recipient_{i}@server.ru"
+            })
+            .ToList();
 
-                Servers = new List<Server>(
-                    Enumerable.Range(1, 10)
-                    .Select(i => new Server
-                    {
-                        Address = $"smtp.server{i}.ru",
-                        Login = $"Login-{i}",
-                        Password = TextEncoder.Encode($"Password-{i}"),
-                        UseSSL = i % 2 == 0
-                    }).ToList()),
+        public static List<Server> Servers { get; } = Enumerable.Range(1, 10)
+            .Select(i => new Server
+            {
+                Name = $"Server {i}",
+                Address = $"smtp.server{i}.com",
+                Login = $"Login-{i}",
+                Password = TextEncoder.Encode($"Password-{i}"),
+                UseSSL = i % 2 == 0
+            })
+            .ToList();
 
-                Messages = new List<Message>(
-                    Enumerable.Range(5, 30)
-                    .Select(i => new Message
-                    {
-                        Subject = $"Сообщение {i}",
-                        Body = $"Текст сообщения {i}"
-                    }).ToList())
-            };
-        }
+        public static List<Message> Messages { get; } = Enumerable.Range(1, 20)
+            .Select(i => new Message
+            {
+                Subject = $"Сообщение {i}",
+                Body = $"Текст сообщения {i}"
+            })
+            .ToList();
     }
 }
